0ea015778b39cec5fb4a7a0839b33d3ee859ce17
Ramadan <ramco95@abv.bg>
2021-09-28 09:51:26 +0300

    Adding BMI Calculator page

--- server.js
+++ server.js
@@ -17,6 +17,32 @@ app.post("/", function(req, res){
     res.send("The result is: " + result);
 });
 
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res){
+    var weight = Number(req.body.w);
+    var height = Number(req.body.h);
+    var bmi = weight / (height ** 2);
+    bmi = bmi.toFixed(1);
+    var result = "";
+
+    if (bmi <= 18.5) {
+        result = "Underweight";
+    } else if (bmi <= 24.9) {
+        result = "Normal weight";
+    } else if (bmi <= 29.9) {
+        result = "Overweight";
+    } else {
+        result = "Obesity";
+    }
+
+    res.send("Your BMI is " + bmi + ": (" + result + ")");
+});
+
+
+
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
 });
